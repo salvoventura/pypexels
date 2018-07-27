7cd99e36e7bc99583b7b7dccfe1890b69a7e54e3
Josh Klar <josh@lumen5.com>
2018-07-26 17:50:35 -0700

    Stop fighting Travis: Move to CircleCI where we already build

--- python2-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# I cannot wait for the day Python 2 is finally nuked from orbit
-# Anyway, flit doesn't work on Python 2, so build some rough
-# approximation of our Python 3 builds
-if hash python2 2>/dev/null; then
-	cat Pipfile | grep -v flit > Pipfile2
-	mv Pipfile2 Pipfile
-fi
